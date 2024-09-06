92fe1cc5ac164f31ea6f302dab6770bcdd588ada
danisoliva <danoliva@outlook.com>
2024-09-06 12:13:22 -0600

    Se creo el proyecto y se agregaron txtfields y botones

---
+++ src/Principal/Principal.java
@@ -0,0 +1,12 @@
+package Principal;
+
+import javax.swing.*;
+
+public class Principal {
+    private JPanel Principal;
+    private JTextField txtNum1;
+    private JTextField txtNum2;
+    private JButton btnSuma;
+    private JButton btnResta;
+    private JButton btnMultiplicacion;
+}
