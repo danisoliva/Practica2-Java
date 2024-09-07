9cf49771376fa042f8300a33fedb583d0477ba03
danoliva <danoliva@outlook.com>
2024-09-06 21:19:57 -0600

    Se agregaron los botones y sus actionListener para cada una de las operaciones

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,6 +1,8 @@
 package Principal;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Principal {
     private JPanel Principal;
@@ -9,4 +11,43 @@ public class Principal {
     private JButton btnSuma;
     private JButton btnResta;
     private JButton btnMultiplicacion;
+    private JButton btnDivision;
+
+    public Principal() {
+        btnDivision.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              double a = Double.parseDouble(txtNum1.getText());
+              double b = Double.parseDouble(txtNum2.getText());
+
+              double c = a / b;
+
+            }
+        });
+        btnMultiplicacion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double a = Double.parseDouble(txtNum1.getText());
+                double b = Double.parseDouble(txtNum2.getText());
+                double c = a * b;
+            }
+        });
+        btnResta.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double a = Double.parseDouble(txtNum1.getText());
+                double b = Double.parseDouble(txtNum2.getText());
+
+                double c = a-b;
+            }
+        });
+        btnSuma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double a = Double.parseDouble(txtNum1.getText());
+                double b = Double.parseDouble(txtNum2.getText());
+                double c = a + b;
+            }
+        });
+    }
 }
